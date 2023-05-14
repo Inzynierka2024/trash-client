37141a815289236cf9c85f7bfad823e845d82336
vis256 <mateusz10077@gmail.com>
2023-05-14 13:15:41 +0200

    Remove title header from screens

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -5,39 +5,52 @@ import { NavigationContainer } from "@react-navigation/native";
 import { ProfileContainer } from "./Views/Profile/ProfileContainer";
 import { RankingContainer } from "./Views/Ranking/RankingContainer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
-UIManager
-
+UIManager;
 
 const Tab = createBottomTabNavigator();
 
 export const Main = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Ranking" 
-                    component={RankingContainer}
-                    options={{
-                      tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="Trophy" color={color} size={size} />
-                      ),
-                    }} />
-        <Tab.Screen name="Mapa" 
-                    component={MapContainer}
-                    options={{
-                      tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="compass" color={color} size={size} />
-                      ),
-                    }} />
-        <Tab.Screen name="Profil" 
-                    component={ProfileContainer}
-                    options={{
-                      tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="user" color={color} size={size} />
-                      ),
-                    }} />
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen
+          name="Ranking"
+          component={RankingContainer}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name="Trophy" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Mapa"
+          component={MapContainer}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="compass"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profil"
+          component={ProfileContainer}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name="user" color={color} size={size} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );

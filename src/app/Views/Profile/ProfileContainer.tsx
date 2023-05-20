682ceaf3674ee5bd5b502c0a555d1d4e06a22256
vanya.haidov <vanya.haidov@gmail.com>
2023-05-20 16:27:05 +0200

    initial login form is created.

--- src/app/Views/Profile/ProfileContainer.tsx
+++ src/app/Views/Profile/ProfileContainer.tsx
@@ -1,22 +1,36 @@
 import { useContext } from "react";
-import { View, Text } from "react-native";
+import styles from "./style";
+import { Alert, Image, Keyboard, KeyboardAvoidingView, Text, TextInput, TouchableWithoutFeedback, Pressable, View} from "react-native";
 import { ThemeContext } from "../../../theme/theme";
 import Ionicons from 'react-native-ionicons';
 
+import { Button, SocialIcon } from "react-native-elements";
+import * as Facebook from "expo-facebook";
+
+const appId = "1047121222092614";
 
 export const ProfileContainer = () => {
   const themeFromContext = useContext(ThemeContext);
+  const onLoginPress = () => {};
+
+  const onFbLoginPress = async () => {
+    
+    };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: themeFromContext.colors.background,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text>Profile View</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.containerView} behavior="padding">
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.loginScreenContainer}>
+          <View style={styles.loginFormView}>
+            <Text style={styles.logoText}>Litter App</Text>
+            <TextInput placeholder="Username" style={styles.loginFormTextInput} />
+            <TextInput placeholder="Password" style={styles.loginFormTextInput} secureTextEntry={true} />
+            <Button buttonStyle={styles.loginButton} onPress={() => onLoginPress()} title="Login" />
+            <Button containerStyle={styles.fbLoginButton} type='clear' onPress={() => onFbLoginPress()} title="Login With Facebook" />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
+

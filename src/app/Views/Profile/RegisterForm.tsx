9dfbb6f5b18f7e87d2b0cab3de4dafa9358d2aee
vis256 <mateusz10077@gmail.com>
2023-08-12 14:14:00 +0200

    Add missing field and handle errors

--- src/app/Views/Profile/RegisterForm.tsx
+++ src/app/Views/Profile/RegisterForm.tsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
-import { Button, TextInput, View, Alert, StyleSheet } from 'react-native';
-import { useAuth } from '../../Logic/AuthContext';
-import { useNavigation } from '@react-navigation/native';
-import axios from 'axios';
-import get_api_url from '../../Utils/get_api_url';
+import React, { useState } from "react";
+import { Button, TextInput, View, Alert, StyleSheet } from "react-native";
+import { useAuth } from "../../Logic/AuthContext";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import get_api_url from "../../Utils/get_api_url";
 
 const RegisterForm: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { register } = useAuth();
   const navigation = useNavigation();
-  
+
   const URL = `http://${get_api_url()}/signup`;
 
   const handleRegister = async () => {
@@ -18,15 +19,32 @@ const RegisterForm: React.FC = () => {
       // Modify this URL to match your API endpoint
       const response = await axios.post(URL, {
         username,
-        password
+        email,
+        password,
       });
-      
-      if (response.data && response.data.token) {
-        register(response.data.token);
-        Alert.alert("Success", "Registration successful!");
-        navigation.navigate('Profile');  // Navigate to profile after registering
-      } else {
-        Alert.alert("Register Error", "Failed to register");
+
+      switch (response.status) {
+        case 201:
+          // Success
+          // TODO: make a login request here and get the token for later
+          const token = "";
+          register(token);
+          navigation.navigate("Profile"); // Navigate to profile after registering
+
+          break;
+
+        case 202:
+          // User already exists
+          Alert.alert(response.data.message);
+          break;
+
+        case 500:
+          // Incorrect data
+          Alert.alert(response.data.message);
+          break;
+
+        default:
+          break;
       }
     } catch (error) {
       Alert.alert("Error", error.message || "There was an error registering.");
@@ -43,6 +61,12 @@ const RegisterForm: React.FC = () => {
       />
       <TextInput
         style={styles.input}
+        placeholder="Email"
+        onChangeText={setEmail}
+        value={email}
+      />
+      <TextInput
+        style={styles.input}
         placeholder="Password"
         onChangeText={setPassword}
         value={password}
@@ -56,16 +80,16 @@ const RegisterForm: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
   },
   input: {
-    width: '100%',
+    width: "100%",
     height: 40,
     marginBottom: 16,
     paddingHorizontal: 12,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     borderRadius: 4,
   },

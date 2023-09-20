ed0b1639f04cc67ec375c06e31c2a7192bd82ad2
Olga Gerlich <ogerlich151@gmail.com>
2023-09-20 21:56:22 +0200

    Change trash endpoints to match specification

--- src/app/Logic/API/get_trash_photo.js
+++ src/app/Logic/API/get_trash_photo.js
@@ -1,20 +1,18 @@
 export default async function (API_URL, id) {
-  const URL = `http://${API_URL}/get_image`;
+  const URL = `http://${API_URL}/garbage/${id}/image/`;
   console.log("Fetching:", URL);
 
   const response = await fetch(URL, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     credentials: "same-origin", // include, *same-origin, omit
     headers: {
       "Content-Type": "application/json",
+      "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwdWJsaWNfaWQiOiJkZWRmYjJiYy0wMDgxLTQxODctODJjZS1jNjFiZjc4MjgzNmIiLCJleHAiOjE2OTUyMzI1MTN9.Fuk8jryxS-gVVwhmyLfgRPbN1cXxF9DRo78TITXodVI"
     },
     redirect: "follow", // manual, *follow, error
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify({
-      id,
-    }), // body data type must match "Content-Type" header
   });
 
   // console.log(response);

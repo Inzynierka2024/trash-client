9e3caf4a5ba3144100ef1bceeac4474e6382c0d4
vis256 <mateusz10077@gmail.com>
2023-09-20 21:27:06 +0200

    Update URL in login

--- src/app/Views/Profile/LoginForm.tsx
+++ src/app/Views/Profile/LoginForm.tsx
@@ -1,28 +1,41 @@
-import React, { useState, useRef } from 'react';
-import { Animated, Alert, Button, TextInput, View, Text, TouchableOpacity, StyleSheet, Image, Dimensions } from 'react-native';
-import { useAuth } from '../../Logic/AuthContext';
-import { useNavigation } from '@react-navigation/native';
-import axios from 'axios';
-import get_api_url from '../../Utils/get_api_url';
+import React, { useState, useRef } from "react";
+import {
+  Animated,
+  Alert,
+  Button,
+  TextInput,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Dimensions,
+} from "react-native";
+import { useAuth } from "../../Logic/AuthContext";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import get_api_url from "../../Utils/get_api_url";
 import logo from "../../../../assets/litter-looter-high-resolution-logo-color-on-transparent-background.png";
 
-const { width: screenWidth } = Dimensions.get('window');
+const { width: screenWidth } = Dimensions.get("window");
 
 const LoginForm: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login } = useAuth();
   const navigation = useNavigation();
 
   const animatePress = useRef(new Animated.Value(1)).current;
-  const URL = `http://${get_api_url()}/login`;
+  const URL = `http://${get_api_url()}/user/login`;
 
   const handleLogin = async () => {
     try {
       const response = await axios.post(URL, {
         email,
-        password
+        password,
       });
+
+      console.log("????", response);
 
       switch (response.status) {
         case 200:
@@ -46,21 +59,21 @@ const LoginForm: React.FC = () => {
     Animated.timing(animatePress, {
       toValue: 0.95,
       duration: 200,
-      useNativeDriver: true
+      useNativeDriver: true,
     }).start();
-  }
+  };
 
   const animateOut = () => {
     Animated.timing(animatePress, {
       toValue: 1,
       duration: 200,
-      useNativeDriver: true
+      useNativeDriver: true,
     }).start(handleLogin);
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <Image source={logo} style={styles.logo} resizeMode="contain"/> 
+      <Image source={logo} style={styles.logo} resizeMode="contain" />
 
       <TextInput
         style={styles.input}
@@ -79,12 +92,13 @@ const LoginForm: React.FC = () => {
         <TouchableOpacity
           onPressIn={animateIn}
           onPressOut={animateOut}
-          style={styles.button}>
+          style={styles.button}
+        >
           <Animated.Text style={styles.buttonText}>Login</Animated.Text>
         </TouchableOpacity>
       </Animated.View>
       <Text style={styles.text}>Don't have an account? </Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
         <Text style={[styles.text, styles.link]}>Register here</Text>
       </TouchableOpacity>
     </View>
@@ -93,44 +107,44 @@ const LoginForm: React.FC = () => {
 
 const styles = StyleSheet.create({
   logo: {
-    width: screenWidth - 32,  
-    height: (screenWidth - 32) * 0.5, 
-    marginBottom: 20, 
+    width: screenWidth - 32,
+    height: (screenWidth - 32) * 0.5,
+    marginBottom: 20,
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
   },
   input: {
-    width: '100%',
+    width: "100%",
     height: 40,
     marginBottom: 16,
     paddingHorizontal: 12,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     borderRadius: 4,
   },
   button: {
-    backgroundColor: '#3d9970',
+    backgroundColor: "#3d9970",
     padding: 10,
     borderRadius: 4,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 150
+    alignItems: "center",
+    justifyContent: "center",
+    width: 150,
   },
   buttonText: {
-    color: '#ffffff',
-    fontWeight: 'bold'
+    color: "#ffffff",
+    fontWeight: "bold",
   },
   text: {
     fontSize: 16,
     marginBottom: 8,
-    fontFamily: 'reem-kufi-regular',
+    fontFamily: "reem-kufi-regular",
   },
   link: {
-    color: 'blue',
+    color: "blue",
   },
 });
 

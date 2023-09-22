efb9339365c6f2efc852ce73fb9cf0fd5d95e9e9
vis256 <mateusz10077@gmail.com>
2023-09-22 12:22:22 +0200

    Refactor and bring fetching trash in area

---
+++ src/app/Logic/API/get_trash_in_area.js
@@ -0,0 +1,24 @@
+import _fetch from "./_fetch";
+
+export default async function (API_URL, bounds) {
+  const c1 = bounds[0];
+  const c2 = bounds[1];
+
+  const beglng = c1[0];
+  const beglat = c1[1];
+
+  const endlng = c2[0];
+  const endlat = c2[1];
+  const URL =
+    `http://${API_URL}/garbage?` +
+    new URLSearchParams({
+      beglat,
+      beglng,
+
+      endlat,
+      endlng,
+    });
+
+  const result = await _fetch(URL, "GET", {});
+  return result;
+}

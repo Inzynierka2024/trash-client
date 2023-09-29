8cb3b069202987abfcf1364fba26687cd99e0102
vis256 <mateusz10077@gmail.com>
2023-09-29 12:54:40 +0200

    Animate buttons for adding new stuff to map

--- src/app/Views/Map/Buttons/AddNew.tsx
+++ src/app/Views/Map/Buttons/AddNew.tsx
@@ -1,7 +1,8 @@
-import { useContext, useState } from "react";
-import { Pressable, StyleSheet, View } from "react-native";
+import { useContext, useState, useRef } from "react";
+import { Animated, Easing, Pressable, StyleSheet, View } from "react-native";
 import { ThemeContext, palette } from "../../../../theme/theme";
 import { FontAwesome } from "@expo/vector-icons";
+import AnimatedPressable from "../../../Animated/AnimatedPressable";
 
 export const AddNewButton = (props: {
   newTrash: Function;
@@ -11,56 +12,118 @@ export const AddNewButton = (props: {
 
   const [open, setOpen] = useState(false);
 
-  const iconSize = 36;
+  const openRadius = 8;
+  const iconSize = 40;
 
-  function onPress() {
-    setOpen(!open);
-  }
+  // Menu button open animations
+  const radiusAnim = useRef(new Animated.Value(iconSize)).current;
+  const canAnim = useRef(new Animated.Value(0)).current;
+  const garbageAnim = useRef(new Animated.Value(0)).current;
+
+  const menuOpen = () => {
+    Animated.timing(radiusAnim, {
+      toValue: openRadius,
+      delay: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(canAnim, {
+      toValue: -132,
+      delay: 0,
+      easing: Easing.sin,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(garbageAnim, {
+      toValue: -66,
+      delay: 0,
+      easing: Easing.sin,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const menuClose = () => {
+    Animated.timing(radiusAnim, {
+      toValue: iconSize,
+      delay: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(canAnim, {
+      toValue: 0,
+      delay: 0,
+      easing: Easing.sin,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(garbageAnim, {
+      toValue: 0,
+      delay: 0,
+      easing: Easing.sin,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
 
   return (
     <View style={styles.container}>
-      <Pressable
-        style={{
-          ...styles.button,
-          ...styles.garbagecan,
-          borderRadius: iconSize,
-          display: open ? "flex" : "none",
-        }}
+      <AnimatedPressable
+        style={[
+          styles.button,
+          styles.garbagecan,
+          {
+            borderRadius: iconSize,
+            transform: [{ translateY: canAnim }],
+          },
+        ]}
         onPress={() => {
           props.newTrash();
           setOpen(false);
         }}
       >
         <FontAwesome name="recycle" size={iconSize} style={styles.icon} />
-      </Pressable>
+      </AnimatedPressable>
 
-      <Pressable
-        style={{
-          ...styles.button,
-          ...styles.trash,
-          borderRadius: iconSize,
-          display: open ? "flex" : "none",
-        }}
+      <AnimatedPressable
+        style={[
+          styles.button,
+          styles.trash,
+          {
+            borderRadius: iconSize,
+            transform: [{ translateY: garbageAnim }],
+          },
+        ]}
         onPress={() => {
           props.newTrash();
           setOpen(false);
         }}
       >
         <FontAwesome name="trash" size={iconSize} style={styles.icon} />
-      </Pressable>
+      </AnimatedPressable>
 
-      <Pressable
-        style={{
-          ...styles.button,
-          ...styles.menu,
-          borderRadius: open ? 8 : iconSize,
-        }}
+      <AnimatedPressable
+        style={[
+          styles.button,
+          styles.menu,
+          {
+            borderRadius: radiusAnim,
+          },
+        ]}
         onPress={() => {
-          onPress();
+          const toOpen = !open;
+
+          setOpen(toOpen);
+          if (toOpen) menuOpen();
+          else menuClose();
         }}
       >
         <FontAwesome name="plus" size={iconSize} style={styles.icon} />
-      </Pressable>
+      </AnimatedPressable>
     </View>
   );
 };
@@ -75,7 +138,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     paddingTop: 10,
-    width: 54,
+    width: 58,
+    position: "absolute",
+    right: 0,
+    bottom: 0,
   },
   icon: {
     marginRight: 0,

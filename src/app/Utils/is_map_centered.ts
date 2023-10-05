7232d4b0481c72b1c423d9a3e364c7bbfcbfe1f1
vis256 <mateusz10077@gmail.com>
2023-10-05 21:49:27 +0200

    Better centering detection

--- src/app/Utils/is_map_centered.ts
+++ src/app/Utils/is_map_centered.ts
@@ -1,14 +1,19 @@
 import MapLibreGL from "@maplibre/maplibre-react-native";
+import calculate_distance from "./calculate_distance";
 
 export default async function (
   center: any,
   userState: MapLibreGL.Location
 ): Promise<boolean> {
-  const EPSILON = 0.0016;
+  const distance = calculate_distance(
+    center[1],
+    center[0],
+    userState.coords.latitude,
+    userState.coords.longitude
+  );
 
-  const result =
-    Math.abs(center[0] - userState.coords.longitude) < EPSILON &&
-    Math.abs(center[1] - userState.coords.latitude) < EPSILON;
+  console.log(distance);
 
+  const result = distance < 0.1;
   return result;
 }

e8d197a0cd945ad4fc581baa6f56783da8e1d689
vis256 <mateusz10077@gmail.com>
2023-10-28 11:30:30 +0200

    Add types for status and bin types

---
+++ src/app/Models/BinStatus.ts
@@ -0,0 +1,1 @@
+export type BinStatus = "empty" | "normal" | "full" | "overfilled"
---
+++ src/app/Models/BinTypes.ts
@@ -0,0 +1,1 @@
+export type BinTypes = "general" | "bio" | "plastic" | "paper" | "glass" | "e-waste" | "pszok" | "debris" | "cloth";

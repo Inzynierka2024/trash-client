bd77beef60bef37039de2d26517f8c7ef4ef8436
vis256 <mateusz10077@gmail.com>
2023-12-04 14:46:34 +0100

    Fix image size tho it might now be too tall mby?

--- src/app/Views/Card/ElementCard.tsx
+++ src/app/Views/Card/ElementCard.tsx
@@ -1,117 +1,125 @@
-import {View, Text, Image, StyleSheet} from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import round from "../../Utils/round";
-import {ElementTypes} from "../../Models/ElementTypes";
-import {useContext} from "react";
-import {palette, ThemeContext} from "../../../theme/theme";
-import {ElementNames} from "../../Models/ElementNames";
-import {ElementIcons} from "../../Models/ElementIcons";
-import {ElementColors} from "../../Models/ElementColors";
-import {BinStatus, BinStatusNames} from "../../Models/BinStatus";
+import { ElementTypes } from "../../Models/ElementTypes";
+import { useContext } from "react";
+import { palette, ThemeContext } from "../../../theme/theme";
+import { ElementNames } from "../../Models/ElementNames";
+import { ElementIcons } from "../../Models/ElementIcons";
+import { ElementColors } from "../../Models/ElementColors";
+import { BinStatus, BinStatusNames } from "../../Models/BinStatus";
 
 export const ElementCard = (props: {
-    type: ElementTypes,
-    imageEnabled: boolean,
-    imageData?: string,
-    distance?: number,
-    timestamp?: Date,
-    addedBy?: string
-    binStatus?: BinStatus
+  type: ElementTypes;
+  imageEnabled: boolean;
+  imageData?: string;
+  distance?: number;
+  timestamp?: Date;
+  addedBy?: string;
+  binStatus?: BinStatus;
 }) => {
-    const themeFromContext = useContext(ThemeContext);
-    const textColor = themeFromContext.colors.primaryText
-    const backgroundColor = themeFromContext.colors.background
+  const themeFromContext = useContext(ThemeContext);
+  const textColor = themeFromContext.colors.primaryText;
+  const backgroundColor = themeFromContext.colors.background;
 
-    return (
-        <View style={styles.container}>
-            {props.imageEnabled === true && (
-                <Image
-                    source={{
-                        uri: "data:image/jpg;base64," + props.imageData,
-                    }}
-                    style={styles.image}
-                />
-            )}
+  return (
+    <View style={styles.container}>
+      {props.imageEnabled === true && (
+        <Image
+          source={{
+            uri: "data:image/jpg;base64," + props.imageData,
+          }}
+          style={styles.image}
+        />
+      )}
 
-            <View style={styles.row}>
-                <Image
-                    style={[styles.elementIcon, {backgroundColor: ElementColors[props.type]}]}
-                    source={ElementIcons[props.type]}/>
+      <View style={styles.row}>
+        <Image
+          style={[
+            styles.elementIcon,
+            { backgroundColor: ElementColors[props.type] },
+          ]}
+          source={ElementIcons[props.type]}
+        />
 
-                <View style={[styles.info, {backgroundColor}]}>
-                    <Text numberOfLines={1} style={{
-                        color: textColor,
-                        fontWeight: "600",
-                        fontSize: 17,
-                    }}>{ElementNames[props.type]}</Text>
+        <View style={[styles.info, { backgroundColor }]}>
+          <Text
+            numberOfLines={1}
+            style={{
+              color: textColor,
+              fontWeight: "600",
+              fontSize: 17,
+            }}
+          >
+            {ElementNames[props.type]}
+          </Text>
 
-                    {props.distance && (
-                        <Text
-                            style={{
-                                color: textColor,
-                            }}
-                        >
-                            Odległość: {props.distance !== -1 ? round(props.distance, 2) : "-"}km
-                        </Text>
-                    )}
+          {props.distance && (
+            <Text
+              style={{
+                color: textColor,
+              }}
+            >
+              Odległość:{" "}
+              {props.distance !== -1 ? round(props.distance, 2) : "-"}km
+            </Text>
+          )}
 
+          {props.addedBy && (
+            <Text
+              style={{
+                color: textColor,
+              }}
+              numberOfLines={1}
+            >
+              Dodane przez: {props.addedBy}
+            </Text>
+          )}
 
-                    {props.addedBy && (
-                        <Text
-                            style={{
-                                color: textColor,
-                            }}
-                            numberOfLines={1}
-                        >
-                            Dodane przez: {props.addedBy}
-                        </Text>
-                    )}
+          {props.timestamp && (
+            <Text
+              style={{
+                color: textColor,
+              }}
+            >
+              Data: {props.timestamp.toLocaleString()}
+            </Text>
+          )}
 
-                    {props.timestamp && (
-                        <Text
-                            style={{
-                                color: textColor,
-                            }}
-                        >
-                            Data: {props.timestamp.toLocaleString()}
-                        </Text>
-                    )}
-
-                    {props.binStatus && (
-                        <Text
-                            style={{
-                                color: textColor,
-                            }}
-                        >
-                            Stan: {BinStatusNames[props.binStatus]}
-                        </Text>
-                    )}
-                </View>
-            </View>
+          {props.binStatus && (
+            <Text
+              style={{
+                color: textColor,
+              }}
+            >
+              Stan: {BinStatusNames[props.binStatus]}
+            </Text>
+          )}
         </View>
-    )
-}
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        width: 260
-    },
-    image: {
-        aspectRatio: "1 / 1",
-        resizeMode: "contain",
-        height: 260,
-        width: 260,
-        backgroundColor: "black"
-    },
-    row: {
-        flexDirection: "row",
-        height: 80,
-    },
-    elementIcon: {
-        height: 80,
-        width: 80,
-    },
-    info: {
-        flex: 1,
-        padding: 6,
-    }
+  container: {
+    width: 260,
+  },
+  image: {
+    aspectRatio: "3 / 4",
+    resizeMode: "contain",
+    width: 260,
+    backgroundColor: "black",
+  },
+  row: {
+    flexDirection: "row",
+    height: 80,
+  },
+  elementIcon: {
+    height: 80,
+    width: 80,
+  },
+  info: {
+    flex: 1,
+    padding: 6,
+  },
 });

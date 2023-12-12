d94657d206a0dbd71da9098979d9574688c7f785
IHaidov <vanya.haidov@gmail.com>
2023-12-13 00:40:40 +0100

    Scoreboard refactoring

---
+++ src/app/Logic/API/get_all_scoreboard.ts
@@ -0,0 +1,12 @@
+import _fetch from "./_fetch";
+
+export default async function () {
+    const URL = `/ranking`;
+    //console.log("Fetching:", URL);
+    
+    const response = await _fetch(URL, "GET", {});
+  
+    //console.log("get_all_scoreboard: ",response);
+    return response;
+  }
+  

6d86eebc0ab29367ae1e39adfb718d4708c3e9ea
vis256 <mateusz10077@gmail.com>
2023-12-14 14:13:02 +0100

    Fix

--- src/app/Views/Map/Buttons/AddNew.tsx
+++ src/app/Views/Map/Buttons/AddNew.tsx
@@ -392,6 +392,7 @@ export const AddNewButton = (props: {
         onPress={() => {
           props.newTrash();
           setOpen(false);
+          setContainerOpen(false);
           menuClose();
         }}
       >

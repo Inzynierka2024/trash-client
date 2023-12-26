d78b5f8e625ed779461d0228c5967e9b460d42e0
IHaidov <vanya.haidov@gmail.com>
2023-12-26 13:23:05 +0100

    User stats form: working tabview with collected and reported garbage adds.

--- src/app/Views/Profile/ProfileStatsForm.tsx
+++ src/app/Views/Profile/ProfileStatsForm.tsx
@@ -5,11 +5,8 @@ import {
   Image,
   StyleSheet,
   TouchableOpacity,
-  ToastAndroid,
   useColorScheme,
-  Platform,
   Dimensions,
-  ScrollView,
 } from "react-native";
 import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 import { Icon } from "react-native-elements";
@@ -17,7 +14,6 @@ import { ThemeContext, darkTheme, palette, theme } from "../../../theme/theme";
 import { useNavigation } from "@react-navigation/native";
 import { useAuth } from "../../Logic/AuthContext";
 import get_user_data from "../../Logic/API/get_user_data";
-import ll_icon from "../../../../assets/litter-looter/adaptive.png";
 import gmailIcon from "../../../../assets/profile/gmail.png";
 import locationIcon from "../../../../assets/profile/home.png";
 import passwordIcon from "../../../../assets/profile/password.png";
@@ -25,8 +21,6 @@ import profileIcon from "../../../../assets/profile/profile.png";
 import pointsIcon from "../../../../assets/profile/coin.png";
 import { useIsFocused } from "@react-navigation/native";
 import get_user_bins from "../../Logic/API/get_user_bins";
-import { BinMetadata } from "../../Models/BinMetadata";
-import { ElementCard } from "../Card/ElementCard";
 import calculate_distance from "../../Utils/calculate_distance";
 import MapLibreGL from "@maplibre/maplibre-react-native";
 import { UserTrashMetadata } from "../../Models/UserTrashMetadata";
@@ -42,7 +36,7 @@ export const ProfileStatsForm = () => {
   const [userState, setUserState] = useState < MapLibreGL.Location > ({
     coords: { latitude: 0, longitude: 0 },
   });
-  const [collectedData, setCollectedData] = useState <UserTrashMetadata[]>([]);
+  const [collectedData, setCollectedData] = useState < UserTrashMetadata[] > ([]);
   const [reportedData, setReportedData] = useState < UserTrashMetadata[] > ([]);
 
   const [user, setUserData] = useState({
@@ -62,6 +56,11 @@ export const ProfileStatsForm = () => {
     navigation.navigate("ProfileEdit");
   };
 
+  const TimestampToDate = (timestamp) => {
+    const date = new Date(timestamp);
+    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+  };
+  
   async function getUser() {
     const result = await get_user_data(state.token);
     if (result.isOk) {
@@ -103,7 +102,7 @@ export const ProfileStatsForm = () => {
         const collectedParsed = tempGarbage.data.collected.map(item => get_garbage_metadata(item));
         setReportedData(tempGarbage.data.added as UserTrashMetadata[]);
         setCollectedData(tempGarbage.data.collected as UserTrashMetadata[]);
-        
+
       }
     }
     fetchData();
@@ -129,19 +128,24 @@ export const ProfileStatsForm = () => {
     { key: "reported", title: "Zgłoszone Odpady" },
   ]);
 
-  const renderItem = ({ item }: { item: any }) => (
-    <View style={{ padding: 10, margin: 10, backgroundColor: 'lightgrey' }}>
-      
-      {/* Assuming the picture is a base64 encoded image */}
-      <Image source={{ uri: `data:image/jpeg;base64,${item.picture}` }} style={{ width: 100, height: 100 }} />
-      <Text>Zgłoszono: {item.collection_timestamp}</Text>
-      <Text>Zgłoszono przez: {item.creation_username}</Text>
-      <Text>Zebrano: {item.created_timestamp}</Text>
-      <Text>Zebrano przez: {item.collection_username}</Text>
-      <Text>Lokacja: {item.latitude}, {item.longitude}</Text>
-      {/* Add more fields as necessary */}
+  const renderItem = ({ item }) => (
+    <View style={styles.tile}>
+      <Image
+        source={{ uri: `data:image/jpeg;base64,${item.picture}` }}
+        style={{ width: 100, height: 100, borderRadius: 10 }}
+      />
+      <View style={styles.tileRow}>
+        <Text style={styles.tileText}>Zgłoszono: {item.creation_username}</Text>
+        <Text style={styles.tileText}>{TimestampToDate(item.creation_timestamp)}</Text>
+
+      </View>
+      <View style={styles.tileRow}>
+      <Text style={styles.tileText}>Zebrano: {item.collection_username}</Text>
+        <Text style={styles.tileText}>{TimestampToDate(item.collection_timestamp)}</Text>
+      </View>
     </View>
   );
+  
   const CollectedTab = () => (
     <FlatList
       data={collectedData}
@@ -169,7 +173,7 @@ export const ProfileStatsForm = () => {
     0,
   );
 
-  
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -178,7 +182,7 @@ export const ProfileStatsForm = () => {
     element: {
       flexDirection: "row",
       padding: 10,
-      margin: 2,
+      //margin: 2,
       alignItems: "center",
     },
     emailContainer: {
@@ -190,7 +194,7 @@ export const ProfileStatsForm = () => {
     },
 
     textCentered: {
-      alignSelf: "center", // Center the text vertically within its parent
+      alignSelf: "center", 
       color: themeFromContext.colors.primaryText,
     },
     editIcon: {
@@ -213,8 +217,8 @@ export const ProfileStatsForm = () => {
       marginVertical: 8,
       width: "100%",
       textAlign: "left",
-      borderBottomWidth: 1,
-      borderBottomColor: "#CCCCCC",
+      //borderBottomWidth: 0.5,
+      borderBottomColor: themeFromContext.colors.background,
       paddingBottom: 4,
       fontWeight: "normal",
       marginLeft: 10,
@@ -238,7 +242,7 @@ export const ProfileStatsForm = () => {
       paddingTop: 10,
       justifyContent: "flex-start",
       flexDirection: "row",
-      alignItems: "flex-start", // Align items to the start of the container
+      alignItems: "flex-start", 
     },
     pointsIcon: {
       width: 30,
@@ -261,27 +265,42 @@ export const ProfileStatsForm = () => {
     tabView: {
       flex: 1,
       width: "100%",
+      borderRadius: 15, 
+    //overflow: 'hidden',
     },
     tabScene: {
       flex: 1,
     },
     tile: {
-      height: 100, // Adjust height as needed
-      backgroundColor: "#e0e0e0", // Example background color
+      backgroundColor: themeFromContext.colors.background,
       borderRadius: 10,
-      justifyContent: "center",
-      alignItems: "center",
+      padding: 10,
       marginVertical: 5,
       marginHorizontal: 10,
+      alignItems: 'center',
+    },
+    tileRow: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '100%',
+      marginTop: 5,
     },
     tileText: {
-      color: "black", // Adjust text color as needed
+      color: themeFromContext.colors.primaryText,
+      flex: 1, 
+      marginHorizontal: 2, 
+    },
+    tileTextBold: {
+      color: themeFromContext.colors.primaryText,
+      flex: 1, 
+      marginHorizontal: 2, 
+      fontStyle: 'bold',
     },
   });
 
   return (
     <ThemeContext.Provider value={darkMode ? darkTheme : theme}>
-      {/* <ScrollView style={styles.container}> */}
+      <View style={styles.container}>
         <TouchableOpacity style={styles.editIcon} onPress={navigateToEditForm}>
           <Icon name="edit" size={24} color="#000" />
         </TouchableOpacity>
@@ -329,46 +348,9 @@ export const ProfileStatsForm = () => {
           )}
           style={styles.tabView}
         />
-      {/* </ScrollView> */}
+      </View>
     </ThemeContext.Provider>
   );
 };
 
-const styles = StyleSheet.create({
-  // ... existing styles ...
-  tabView: {
-    flex: 1,
-    width: "100%",
-    marginTop: 5,
-  },
-  tabScene: {
-    flex: 1,
-  },
-  tile: {
-    flexDirection: 'row', // Arrange image and content side by side
-    alignItems: 'center', // Center items vertically
-    //backgroundColor: '#fc4', // White background for the tile
-    borderRadius: 5, // Rounded corners for the tile
-    padding: 10, // Padding inside the tile
-    marginVertical: 8, // Margin between tiles
-    marginHorizontal: 16, // Horizontal margin
-    elevation: 2, // Shadow for the tile
-    //width: 50,
-    //height: 50
-  },
-  tileImage: {
-    width: 80, // Width of the image
-    height: 80, // Height of the image
-    borderRadius: 5, // Rounded corners for the image
-    marginRight: 10, // Margin between image and text content
-  },
-  tileContent: {
-    flex: 1, // Take up remaining space
-  },
-  tileText: {
-    fontSize: 16, // Font size for the text
-    //color: '#333', // Color for the text
-    marginBottom: 4, // Margin between text elements
-  },
-});
 export default ProfileStatsForm;

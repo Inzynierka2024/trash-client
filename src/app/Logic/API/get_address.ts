f491ef9c74dea86ad5dc5a6f0cc9b46578cfc07f
vis256 <67900555+vis256@users.noreply.github.com>
2023-12-29 17:57:57 +0100

    ⚡️ Don't fetch data to individual building level and only to street lvl

--- src/app/Logic/API/get_address.ts
+++ src/app/Logic/API/get_address.ts
@@ -3,7 +3,7 @@ import _fetch from "./_fetch";
 import { Address } from "../../Models/Address";
 
 export default async function (lat, lng): Promise<Address> {
-  const URL = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}&zoom=18`;
+  const URL = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}&zoom=17`;
 
   const result = await fetch(URL, {
     method: "GET",
